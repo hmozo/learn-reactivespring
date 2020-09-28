d3e9b73da2c53ec7386c1e4f95cc59a7de0fdcea
Hugo Mozo <hmozo@outlook.com>
2020-09-28 09:34:37 +0200

    First Flux test

---
+++ learn-reactivespring/src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoTest.java
@@ -0,0 +1,23 @@
+package com.learnreactivespring.fluxandmonoplayground;
+
+import org.junit.jupiter.api.Test;
+
+import reactor.core.publisher.Flux;
+
+public class FluxAndMonoTest {
+	
+	@Test
+	void fluxTest() {
+		
+		Flux<String> stringFlux= Flux.just("Spring", "Spring Boot", "Reactive Spring")
+				//.concatWith(Flux.error(new RuntimeException("Exception occurred")))
+				.concatWith(Flux.just("... after error"))
+				.log();
+		
+		stringFlux
+			.subscribe(System.out::println,
+					e -> System.err.println(e)
+					, () -> System.out.println("Completed"));
+	}
+
+}
