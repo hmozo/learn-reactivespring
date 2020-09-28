441f68485bbfd95ae29e47aec09e590c320bc648
Hugo Mozo <hmozo@outlook.com>
2020-09-28 10:04:44 +0200

    Testing elements in Flux

--- learn-reactivespring/src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoTest.java
+++ learn-reactivespring/src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoTest.java
@@ -1,8 +1,10 @@
 package com.learnreactivespring.fluxandmonoplayground;
 
+
 import org.junit.jupiter.api.Test;
 
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 public class FluxAndMonoTest {
 	
@@ -20,4 +22,45 @@ public class FluxAndMonoTest {
 					, () -> System.out.println("Completed"));
 	}
 
+	
+	@Test
+	void fluxTestElements_withoutError() {
+		Flux<String> stringFlux= Flux.just("Spring", "Spring Boot", "Reactive Spring")
+			.log();
+		
+		StepVerifier.create(stringFlux)
+			.expectNext("Spring")
+			.expectNext("Spring Boot")
+			.expectNext("Reactive Spring")
+			.verifyComplete();
+		
+		
+	}
+	
+	@Test
+	void fluxTestElements_withError() {
+		Flux<String> stringFlux= Flux.just("Spring", "Spring Boot", "Reactive Spring")
+			.concatWith(Flux.error(new RuntimeException("Exception occurred")))
+			.log();
+		
+		StepVerifier.create(stringFlux)
+			.expectNext("Spring")
+			.expectNext("Spring Boot")
+			.expectNext("Reactive Spring")
+			.expectError(RuntimeException.class)
+			.verify();
+	}
+	
+	@Test
+	void fluxTestElementsCount_withError() {
+		Flux<String> stringFlux= Flux.just("Spring", "Spring Boot", "Reactive Spring")
+				.concatWith(Flux.error(new RuntimeException("Exception 253 occurred")))
+				.log();
+			
+		StepVerifier.create(stringFlux)
+			.expectNextCount(3)
+			.expectErrorMessage("Exception 253 occurred")
+			.verify();
+		
+	}
 }
