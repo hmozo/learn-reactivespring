f7df913e71221f6d6bf7df0ca46bcf13a0e6979b
Hugo Mozo <hmozo@outlook.com>
2020-10-27 13:28:30 +0100

    BackPressure test

---
+++ learn-reactivespring/src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoBackpressureTest.java
@@ -0,0 +1,78 @@
+package com.learnreactivespring.fluxandmonoplayground;
+
+import org.junit.jupiter.api.Test;
+
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+public class FluxAndMonoBackpressureTest {
+
+	@Test
+	void backpressuresTest() {
+		
+		Flux<Integer> finiteFlux= Flux.range(1, 10)
+			.log();
+		
+		StepVerifier.create(finiteFlux)
+			.expectSubscription()
+			.thenRequest(1)
+			.expectNext(1)
+			.thenRequest(1)
+			.expectNext(2)
+			.thenCancel()
+			.verify();
+	}
+	
+	@Test
+	void backpressure() {
+		
+		Flux<Integer> finiteFlux= Flux.range(1, 10)
+				.log();
+		
+		finiteFlux.subscribe(
+				element -> System.out.println("Element is: " + element),
+				(e -> System.err.println("Exception is: " + e)),
+				() -> System.out.println("Done"),
+				subscription -> subscription.request(2)
+		);
+	}
+	
+	@Test
+	void backpressure_cancel() {
+		
+		Flux<Integer> finiteFlux= Flux.range(1, 10)
+				.log();
+		
+		finiteFlux.subscribe(
+				element -> System.out.println("Element is: " + element),
+				(e -> System.err.println("Exception is: " + e)),
+				() -> System.out.println("Done"),
+				subscription -> subscription.cancel()
+		);
+	}
+	
+	@Test
+	void backpressure_customized() {
+		
+		Flux<Integer> finiteFlux= Flux.range(1, 10)
+				.log();
+		
+		finiteFlux.subscribe(
+				new BaseSubscriber<Integer>() {
+					@Override
+					protected void hookOnNext(Integer value) {
+						this.request(1);
+						System.out.println("value received is: " + value);
+						if(value == 4) {
+							this.cancel();
+						}
+					};
+				}
+		);
+	}
+
+}
+
+
+
